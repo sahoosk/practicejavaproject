cdb743e48324fe941a836d5ab9c2bfb371fb9508
Santosh Sahoo <santosh@msn.com>
2019-07-21 20:43:43 +0530

    Update ConnectToDB.java

--- src/pkg002_LearnJDBC/ConnectToDB.java
+++ src/pkg002_LearnJDBC/ConnectToDB.java
@@ -1,20 +1,22 @@
-package pkg002_LearnJDBC;
+package pkg_001_TrialWorkSpace;
 
 import java.sql.*;
 
-public class ConnectToDB {
-
+public class HelloWorld {
+	
 	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
 		Connection conn = null;
 		try {
 			
 			String userName = "root";
-			String passWord = "spring";
+			String passWord = "root";
 			
-			String url = "jdbc:mysql://localhost/mydb?useSSL=false";    //?useSSL=false makes the program not to show warning 
+			String url = "jdbc:mysql://localhost/world?useSSL=false";    // world is the name of a valid db and '?useSSL=false' makes the program not to show warning 
 			
-			//Class.forName("com.mysql.jdbc.Driver").newInstance(); driver deprecated
-			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			conn = DriverManager.getConnection(url, userName, passWord);
 			
@@ -38,5 +40,6 @@ public class ConnectToDB {
 			}
 		}
 	}
-	
-}
+		
+	}
+
