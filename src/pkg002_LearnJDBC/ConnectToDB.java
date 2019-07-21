5de9a5d896c1500f7165cc34163378e75fc53054
Santosh Sahoo <santosh@msn.com>
2019-07-21 20:45:43 +0530

    Update ConnectToDB.java

--- src/pkg002_LearnJDBC/ConnectToDB.java
+++ src/pkg002_LearnJDBC/ConnectToDB.java
@@ -43,3 +43,20 @@ public class HelloWorld {
 		
 	}
 
+
+/*
+
+If you're facing this problem with Eclipse, I've been following many different solutions but the one that worked for me is this:
+
+Right click your project folder and open up Properties.
+
+From the right panel, select Java Build Path then go to Libraries tab.
+
+Select Add External JARs to import the mysql driver (here the j connector jar file)
+
+
+You should see the sql driver on the list. Select it and click first.
+
+
+*/
+
